bbaebd9b6fc766d2fe4774831ee585a4cd22067c
Sadek Walid Mendi <Bloodyline01@gmail.com>
2022-02-15 12:24:06 -0500

    Updated file names

--- contracts/mixologist/MixologistHelper.sol
+++ contracts/mixologist/MixologistHelper.sol
@@ -4,30 +4,30 @@ pragma experimental ABIEncoderV2;
 import '@boringcrypto/boring-solidity/contracts/libraries/BoringMath.sol';
 import '@boringcrypto/boring-solidity/contracts/libraries/BoringRebase.sol';
 import '../bar/TapiocaBar.sol';
-import './KashiPair.sol';
+import './Mixologist.sol';
 
-/// @dev This contract provides useful helper functions for `KashiPair`.
-contract KashiPairHelper {
+/// @dev This contract provides useful helper functions for `Mixologist`.
+contract MixologistHelper {
     using BoringMath for uint256;
     using BoringMath128 for uint128;
     using RebaseLibrary for Rebase;
 
     /// @dev Helper function to calculate the collateral shares that are needed for `borrowPart`,
     /// taking the current exchange rate into account.
-    function getCollateralSharesForBorrowPart(KashiPair kashiPair, uint256 borrowPart) public view returns (uint256) {
-        // Taken from KashiPair
+    function getCollateralSharesForBorrowPart(Mixologist mixologist, uint256 borrowPart) public view returns (uint256) {
+        // Taken from Mixologist
         uint256 EXCHANGE_RATE_PRECISION = 1e18;
         uint256 LIQUIDATION_MULTIPLIER = 112000; // add 12%
         uint256 LIQUIDATION_MULTIPLIER_PRECISION = 1e5;
 
-        (uint128 elastic, uint128 base) = kashiPair.totalBorrow();
+        (uint128 elastic, uint128 base) = mixologist.totalBorrow();
         Rebase memory totalBorrow = Rebase(elastic, base);
         uint256 borrowAmount = totalBorrow.toElastic(borrowPart, false);
 
         return
-            kashiPair.tapiocaBar().toShare(
-                kashiPair.collateralId(),
-                borrowAmount.mul(LIQUIDATION_MULTIPLIER).mul(kashiPair.exchangeRate()) /
+            mixologist.tapiocaBar().toShare(
+                mixologist.collateralId(),
+                borrowAmount.mul(LIQUIDATION_MULTIPLIER).mul(mixologist.exchangeRate()) /
                     (LIQUIDATION_MULTIPLIER_PRECISION * EXCHANGE_RATE_PRECISION),
                 false
             );
