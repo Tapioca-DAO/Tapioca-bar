f021b97aeef05892d2d6f0fa2d2705c9799f35e4
Sadek Walid Mendi <Bloodyline01@gmail.com>
2022-02-18 10:53:30 -0500

    Added deployment viewer script

---
+++ scripts/getDeployments.ts
@@ -0,0 +1,17 @@
+import hre from 'hardhat';
+
+async function main() {
+    const {deployments} = hre;
+    const all = (await deployments.all());
+    Object.keys(all).map(async e=> {
+        console.log({[e]: all[e].address});
+    });
+
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
