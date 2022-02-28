92d3aad299c36bbd74a4d478a72ec9f9f81c2976
Sadek Walid Mendi <Bloodyline01@gmail.com>
2022-02-28 12:58:51 -0500

    - Added more utils and test cases

--- test/mixologist.test.ts
+++ test/mixologist.test.ts
@@ -1,12 +1,11 @@
 import { ethers } from 'hardhat';
 import { expect } from 'chai';
 import { register } from './test.utils';
-
 describe('Mixologist test', () => {
     it('Should deposit to bar, add asset to mixologist, remove asset and withdraw', async () => {
-        const { usdc, weth, bar, wethUsdcMixologist, deployer} = await register();
+        const { usdc, weth, bar, wethUsdcMixologist, deployer } = await register();
 
-        const mintVal = ethers.BigNumber.from(1e18.toString()).mul(1e5);
+        const mintVal = ethers.BigNumber.from((1e18).toString()).mul(1e5);
         usdc.freeMint(mintVal);
 
         const balanceBefore = await usdc.balanceOf(deployer.address);
@@ -14,30 +13,74 @@ describe('Mixologist test', () => {
         const mintValShare = await bar.toShare(await wethUsdcMixologist.assetId(), mintVal, false);
         await (await usdc.approve(bar.address, mintVal)).wait();
         await (await bar.deposit(await wethUsdcMixologist.assetId(), deployer.address, deployer.address, 0, mintValShare)).wait();
-        
+
         // Add asset to Mixologist
         await (await bar.setApprovalForAll(wethUsdcMixologist.address, true)).wait();
         await (await wethUsdcMixologist.addAsset(deployer.address, false, mintValShare)).wait();
-        
+
         // Remove asset from Mixologist
         await (await wethUsdcMixologist.removeAsset(deployer.address, mintValShare)).wait();
-        
+
         // Withdraw from bar
         await (await bar.withdraw(await wethUsdcMixologist.assetId(), deployer.address, deployer.address, 0, mintValShare)).wait();
-        
+
         // Check the value of the asset
         const balanceAfter = await usdc.balanceOf(deployer.address);
         expect(balanceAfter).to.equal(balanceBefore);
     });
 
-    it('Should deposit USDC, deposit WETH and borrow USDC', async () => {
-        const { usdc, weth, bar, wethUsdcMixologist, deployer, mixologistHelper, eoa1} = await register();
+    it.only('Should lend Weth, deposit Usdc collateral and borrow Weth', async () => {
+        const {
+            usdc,
+            weth,
+            bar,
+            wethDepositAndAddAsset,
+            usdcDepositAndAddCollateral,
+            eoa1,
+            approveTokensAndSetBarApproval,
+            deployer,
+            wethUsdcMixologist,
+            jumpTime,
+        } = await register();
 
-        const mintVal = ethers.BigNumber.from(1e18.toString()).mul(1e5);
-        usdc.freeMint(mintVal);
-        weth.freeMint(mintVal);
+        const assetId = await wethUsdcMixologist.assetId();
+        const collateralId = await wethUsdcMixologist.collateralId();
+        const wethMintVal = ethers.BigNumber.from((1e18).toString()).mul(10);
+        const usdcMintVal = wethMintVal.mul(2950);
 
+        // We get asset
+        weth.freeMint(wethMintVal);
+        usdc.connect(eoa1).freeMint(usdcMintVal);
 
-        
+        // We approve external operators
+        await approveTokensAndSetBarApproval();
+        await approveTokensAndSetBarApproval(eoa1);
+
+        // We lend WETH as deployer
+        await wethDepositAndAddAsset(wethMintVal);
+        expect(await wethUsdcMixologist.balanceOf(deployer.address)).to.be.equal(await bar.toShare(assetId, wethMintVal, false));
+
+        // We deposit USDC collateral
+        await usdcDepositAndAddCollateral(usdcMintVal, eoa1);
+        expect(await wethUsdcMixologist.userCollateralShare(eoa1.address)).equal(await bar.toShare(collateralId, usdcMintVal, false));
+
+        // We borrow 74% collateral, max is 75%
+        const borrowVal = wethMintVal.mul(12).div(100);
+
+        await wethUsdcMixologist.connect(eoa1).borrow(eoa1.address, borrowVal);
+        await bar.connect(eoa1).withdraw(assetId, eoa1.address, eoa1.address, borrowVal, 0);
+
+        // Can't liquidate
+        await expect(wethUsdcMixologist.liquidate([eoa1.address], [borrowVal], deployer.address, ethers.constants.AddressZero)).to.be
+            .reverted;
+
+        // Can be liquidated after fees accrual
+        await wethUsdcMixologist.setSwapper(ethers.constants.AddressZero, true);
+        await jumpTime(3600 * 60 * 60 * 24 * 60);
+        await wethUsdcMixologist.accrue();
+        console.log(await wethUsdcMixologist.userCollateralShare(eoa1.address));
+        await wethUsdcMixologist.liquidate([eoa1.address], [borrowVal], deployer.address, ethers.constants.AddressZero);
+        await expect(wethUsdcMixologist.liquidate([eoa1.address], [borrowVal], deployer.address, ethers.constants.AddressZero)).to.not.be
+            .reverted;
     });
 });
--- test/test.utils.ts
+++ test/test.utils.ts
@@ -2,10 +2,7 @@ import { BigNumberish } from 'ethers';
 import hre, { ethers } from 'hardhat';
 
 export async function setBalance(addr: string, ether: number) {
-    await ethers.provider.send('hardhat_setBalance', [
-        addr,
-        ethers.utils.hexStripZeros(ethers.utils.parseEther(String(ether))._hex),
-    ]);
+    await ethers.provider.send('hardhat_setBalance', [addr, ethers.utils.hexStripZeros(ethers.utils.parseEther(String(ether))._hex)]);
 }
 
 export async function register() {
@@ -15,7 +12,7 @@ export async function register() {
     const deployer = (await ethers.getSigners())[0];
 
     // Deploy USDC and WETH
-    const usdc = await (await ethers.getContractFactory('ERC20Mock')).deploy(ethers.BigNumber.from(1e18.toString()).mul(1e9));
+    const usdc = await (await ethers.getContractFactory('ERC20Mock')).deploy(ethers.BigNumber.from((1e18).toString()).mul(1e9));
     await usdc.deployed();
     const weth = await (await ethers.getContractFactory('WETH9Mock')).deploy();
     await weth.deployed();
@@ -23,7 +20,7 @@ export async function register() {
     // Deploy WethUSDC mock oracle
     const wethUsdcOracle = await (await ethers.getContractFactory('OracleMock')).deploy();
     await wethUsdcOracle.deployed();
-    await (await wethUsdcOracle.set(ethers.BigNumber.from(2980).mul(1e18.toString()))).wait();
+    await (await wethUsdcOracle.set(ethers.BigNumber.from(2980).mul((1e18).toString()))).wait();
 
     // Deploy Bar
     const bar = await (await ethers.getContractFactory('TapiocaBar')).deploy(weth.address);
@@ -36,27 +33,14 @@ export async function register() {
     const usdcAssetId = await bar.ids(0, usdc.address, ethers.constants.AddressZero, 0);
 
     // Deploy WethUSDC isolated Mixologist pair
-    const wethUsdcMixologist = await (await ethers.getContractFactory('Mixologist')).deploy(
-        bar.address,
-        weth.address,
-        wethAssetId,
-        usdc.address,
-        usdcAssetId,
-        wethUsdcOracle.address,
-    );
+    const wethUsdcMixologist = await (
+        await ethers.getContractFactory('Mixologist')
+    ).deploy(bar.address, weth.address, wethAssetId, usdc.address, usdcAssetId, wethUsdcOracle.address);
     await wethUsdcMixologist.deployed();
 
     // Deploy an EOA
     const eoa1 = new ethers.Wallet(ethers.Wallet.createRandom().privateKey, ethers.provider);
     await setBalance(eoa1.address, 100000);
-
-    // Mixologist base minimum
-    await (await usdc.freeMint(1000)).wait();
-    const mintValShare = await bar.toShare(await wethUsdcMixologist.assetId(), 1000, false);
-    await (await usdc.approve(bar.address, 1000)).wait();
-    await (await bar.deposit(await wethUsdcMixologist.assetId(), deployer.address, deployer.address, 0, mintValShare)).wait();
-    await (await bar.setApprovalForAll(wethUsdcMixologist.address, true)).wait();
-    await (await wethUsdcMixologist.addAsset(deployer.address, false, mintValShare)).wait();
 
     // Helper
     const mixologistHelper = await (await ethers.getContractFactory('MixologistHelper')).deploy();
@@ -72,31 +56,64 @@ export async function register() {
         mixologistHelper,
         eoa1,
     };
-    
+
     /**
      * UTIL FUNCS
      */
-    const approveTokenAndSetBarApproval = async (account: typeof eoa1, nUsdc:BigNumberish, nWeth:BigNumberish)=>{
-        await (await bar.batch([
-            (await usdc.connect(account).populateTransaction.approve(bar.address, nUsdc)).data || '',
-            (await weth.connect(account).populateTransaction.approve(bar.address, nWeth)).data || '',
-            (await bar.connect(account).populateTransaction.setApprovalForAll(wethUsdcMixologist.address, true)).data || '',
-        ], true)).wait();
+    const BN = (n: number | string) => ethers.BigNumber.from(n);
+
+    const mine = async (blocks: number) => {
+        for (let i = 0; i < blocks; i++) {
+            await ethers.provider.send('evm_mine', []);
+        }
     };
 
-    const wethDepositAndAddAsset = async (account: typeof eoa1, nWeth:BigNumberish) =>{
-        const _valShare = await bar.toShare(await wethUsdcMixologist.assetId(), nWeth, false);
-        await (await bar.connect(account).deposit(await wethUsdcMixologist.assetId(), account.address, account.address, 0, _valShare)).wait();
-        await (await wethUsdcMixologist.connect(account).addAsset(account.address, false, _valShare)).wait();
+    const jumpTime = async (ms: number) => {
+        await ethers.provider.send('evm_increaseTime', [ms]);
+        await ethers.provider.send('evm_mine', []);
     };
 
-    const usdcDepositAndAddAsset = async (account: typeof eoa1, nWeth:BigNumberish) =>{
-        const _valShare = await bar.toShare(await wethUsdcMixologist.collateralId(), nWeth, false);
-        await (await bar.connect(account).deposit(await wethUsdcMixologist.collateralId(), account.address, account.address, 0, _valShare)).wait();
-        await (await wethUsdcMixologist.connect(account).addAsset(account.address, false, _valShare)).wait();
+    const approveTokensAndSetBarApproval = async (account?: typeof eoa1) => {
+        const _usdc = account ? usdc.connect(account) : usdc;
+        const _weth = account ? weth.connect(account) : weth;
+        const _bar = account ? bar.connect(account) : bar;
+        await (await _usdc.approve(bar.address, ethers.constants.MaxUint256)).wait();
+        await (await _weth.approve(bar.address, ethers.constants.MaxUint256)).wait();
+        await (await _bar.setApprovalForAll(wethUsdcMixologist.address, true)).wait();
     };
 
-    const utilFuncs = {approveTokenAndSetBarApproval, wethDepositAndAddAsset, usdcDepositAndAddAsset};
+    const wethDepositAndAddAsset = async (amount: BigNumberish, account?: typeof eoa1) => {
+        const _account = account ?? deployer;
+        const _bar = account ? bar.connect(account) : bar;
+        const _wethUsdcMixologist = account ? wethUsdcMixologist.connect(account) : wethUsdcMixologist;
 
-    return {...initialSetup, ...utilFuncs};
+        const id = await _wethUsdcMixologist.assetId();
+        const _valShare = await _bar.toShare(id, amount, false);
+        await (await _bar.deposit(id, _account.address, _account.address, 0, _valShare)).wait();
+        await (await _wethUsdcMixologist.addAsset(_account.address, false, _valShare)).wait();
+    };
+
+    const usdcDepositAndAddCollateral = async (amount: BigNumberish, account?: typeof eoa1) => {
+        const _account = account ?? deployer;
+        const _bar = account ? bar.connect(account) : bar;
+        const _wethUsdcMixologist = account ? wethUsdcMixologist.connect(account) : wethUsdcMixologist;
+
+        const id = await _wethUsdcMixologist.collateralId();
+        await (await _bar.deposit(id, _account.address, _account.address, amount, 0)).wait();
+        const _valShare = await _bar.balanceOf(_account.address, id);
+        await (await _wethUsdcMixologist.addCollateral(_account.address, false, _valShare)).wait();
+    };
+
+    const initContracts = async () => {
+        await (await usdc.freeMint(1000)).wait();
+        const mintValShare = await bar.toShare(await wethUsdcMixologist.assetId(), 1000, false);
+        await (await usdc.approve(bar.address, 1000)).wait();
+        await (await bar.deposit(await wethUsdcMixologist.assetId(), deployer.address, deployer.address, 0, mintValShare)).wait();
+        await (await bar.setApprovalForAll(wethUsdcMixologist.address, true)).wait();
+        await (await wethUsdcMixologist.addAsset(deployer.address, false, mintValShare)).wait();
+    };
+
+    const utilFuncs = { BN, jumpTime, approveTokensAndSetBarApproval, wethDepositAndAddAsset, usdcDepositAndAddCollateral };
+
+    return { ...initialSetup, ...utilFuncs };
 }
