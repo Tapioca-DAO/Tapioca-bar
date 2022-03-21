c6e4f23a85d84e0b1023e07fe8ea1f1afe08a887
0xRektora <bloodyline01@gmail.com>
2022-03-21 10:33:38 -0400

    - New `registeredMasterContract()` modifier - Updated related master contract references -  Implemented `withdrawAllProtocolFees()`

--- contracts/bar/BeachBar.sol
+++ contracts/bar/BeachBar.sol
@@ -7,6 +7,7 @@ import './interfaces/IWrappedNative.sol';
 import './interfaces/IStrategy.sol';
 import './enums/YieldBoxTokenType.sol';
 import '../swappers/MultiSwapper.sol';
+import '../mixologist/Mixologist.sol';
 import '@boringcrypto/boring-solidity/contracts/BoringOwnable.sol';
 
 enum ContractType {
@@ -25,6 +26,8 @@ contract BeachBar is BoringOwnable, YieldBox {
     uint256 public immutable tapAssetId;
 
     MasterContract[] masterContracts;
+
+    // Used to check if a master contract is registered to be used as a Mixologist template
     mapping(address => bool) isMasterContractRegistered;
 
     address public feeTo; // Protocol
@@ -39,6 +42,15 @@ contract BeachBar is BoringOwnable, YieldBox {
     ) YieldBox(wrappedNative_, uriBuilder_) {
         tapToken = tapToken_;
         tapAssetId = uint96(registerAsset(TokenType.ERC20, address(tapToken_), IStrategy(address(0)), 0));
+    }
+
+    // ******************//
+    // *** MODIFIERS *** //
+    // ***************** //
+
+    modifier registeredMasterContract(MasterContract mc) {
+        require(isMasterContractRegistered[mc] == true, 'BeachBar: MC not registered');
+        _;
     }
 
     // ************************ //
@@ -67,33 +79,53 @@ contract BeachBar is BoringOwnable, YieldBox {
         return depositETH(asset.strategy, to, amount);
     }
 
+    /// @notice Loop through the master contracts and call `depositFeesToBeachBar()` to each one of their clones.
+    /// @dev `swappers_` can have one element that'll be used for all clones. Or one swapper per MasterContract.
+    /// @param swappers_ One or more swappers to convert the asset to TAP.
+    function withdrawAllProtocolFees(MultiSwapper[] swappers_) public {
+        require(address(swappers_[0]) != address(0), 'BeachBar: zero address');
+
+        uint256 masterContractLength = masterContracts.length;
+        bool singleSwapper = swappers_.length != masterContractLength;
+
+        address[] memory clonesOf_;
+        // Loop through master contracts.
+        for (uint256 i = 0; i < masterContractLength; ) {
+            clonesOf_ = clonesOf[address(masterContracts[i])];
+            // Loop through clones of the current MC.
+            for (uint256 j = 0; j < clonesOf_.length; ) {
+                Mixologist(clonesOf_[j]).depositFeesToBeachBar(singleSwapper ? swappers_[0] : swappers_[i]);
+                ++j;
+            }
+            ++i;
+        }
+    }
+
     // *********************** //
     // *** OWNER FUNCTIONS *** //
     // *********************** //
 
     /// @notice Register a master contract
-    /// @param masterContract_ The address of the contract
+    /// @param mcAddress The address of the contract
     /// @param contractType_ The risk type of the contract
-    function registerMasterContract(address masterContract_, ContractType contractType_) external onlyOwner {
+    function registerMasterContract(address mcAddress, ContractType contractType_) external onlyOwner {
         MasterContract memory mc;
-        mc.location = masterContract_;
+        mc.location = mcAddress;
         mc.risk = contractType_;
         masterContracts.push(mc);
-        isMasterContractRegistered[masterContract_] = true;
+        isMasterContractRegistered[mcAddress] = true;
     }
 
     /// @notice Register a Mixologist
-    /// @param masterContract_ The address of the master contract which must be already registered
+    /// @param mc The address of the master contract which must be already registered
     /// @param data The init data of the Mixologist
     /// @param useCreate2 Whether to use create2 or not
     function registerMixologist(
-        address masterContract_,
+        address mc,
         bytes calldata data,
         bool useCreate2
-    ) external payable onlyOwner {
-        require(isMasterContractRegistered[masterContract_] == true, 'BeachBar: MC not registered');
-
-        deploy(masterContract_, data, useCreate2);
+    ) external payable onlyOwner registeredMasterContract(mc) {
+        deploy(mc, data, useCreate2);
     }
 
     function setFeeTo(address feeTo_) external onlyOwner {
