6c0b0a1ed2ca47159db6a8de4f13f12e0cde29f0
0xRektora <bloodyline01@gmail.com>
2022-03-21 11:44:55 -0400

    - Removed unnecessary file and bugfix import

--- contracts/swappers/MultiSwapper.sol
+++ contracts/swappers/MultiSwapper.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.9;
 
+import '@boringcrypto/boring-solidity/contracts/libraries/BoringERC20.sol';
 import '../libraries/IUniswapV2Factory.sol';
 import '../libraries/UniswapV2Library.sol';
 import '../libraries/IUniswapV2Pair.sol';
