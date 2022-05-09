c52efc0f4910729bebabdf7a4493c3a3d5c9115f
0xRektora <bloodyline01@gmail.com>
2022-05-09 09:38:00 -0400

    test: Mixologist mock for LQ

---
+++ contracts/liquidationQueue/mocks/LQMixologistMock.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
+
+import '../ILiquidationQueue.sol';
+import '../../bar/BeachBar.sol';
+
+contract LQMixologistMock {
+    BeachBar immutable beachBar;
+
+    uint256 immutable assetId;
+
+    uint256 constant EXCHANGE_RATE_PRECISION = 1e18;
+
+    constructor(BeachBar _beachBar, uint256 _assetId) {
+        beachBar = _beachBar;
+        assetId = _assetId;
+    }
+
+    function initLq(
+        ILiquidationQueue liquidationQueue,
+        LiquidationQueueMeta calldata lqMeta
+    ) external {
+        liquidationQueue.init(lqMeta, EXCHANGE_RATE_PRECISION);
+    }
+}
