acd3faaf14eb8bf73232061430955615cb755c8c
0xRektora <bloodyline01@gmail.com>
2022-06-16 17:01:57 -0400

    Updated deploy script & removed old files

--- deploy/00_deploy_bar.ts
+++ deploy/00_deploy_bar.ts
@@ -1,21 +1,34 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
+import fs from 'fs';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const { deployments, getNamedAccounts } = hre;
     const { deploy } = deployments;
     const { deployer } = await getNamedAccounts();
 
+    let yieldboxAdrr: any = {};
+    try {
+        yieldboxAdrr = JSON.parse(fs.readFileSync('yieldbox.json', 'utf-8'));
+    } catch (e) {}
+
+    const yieldbox = yieldboxAdrr[await hre.getChainId()];
+    const tap = '0x0';
+
+    const args = [yieldbox, tap];
     await deploy('BeachBar', {
         from: deployer,
         log: true,
-        args: ['0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000'],
+        args,
     });
 
     if (hre.network.live || hre.network.tags['rinkeby']) {
         try {
             const bar = await deployments.get('BeachBar');
-            await hre.run('verify', {address: bar.address, constructorArgsParams: ['0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000'] });
+            await hre.run('verify', {
+                address: bar.address,
+                constructorArgsParams: args,
+            });
         } catch (err) {
             console.log(err);
         }
