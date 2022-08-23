e1c767e9494acea5fc0a821af5f85061d9b72cf8
cryptotechmaker <grigore.cosmin@gmail.com>
2022-08-23 09:59:48 +0300

    introduced hardhat-toolbox

--- hardhat.export.ts
+++ hardhat.export.ts
@@ -1,14 +1,10 @@
 import * as dotenv from 'dotenv';
 
+import '@nomicfoundation/hardhat-toolbox';
+import '@nomicfoundation/hardhat-chai-matchers';
 import { HardhatUserConfig } from 'hardhat/config';
-import '@nomiclabs/hardhat-waffle';
-import '@typechain/hardhat';
-import '@nomiclabs/hardhat-etherscan';
 import 'hardhat-deploy';
-import '@nomiclabs/hardhat-ethers';
 import 'hardhat-contract-sizer';
-// import 'hardhat-gas-reporter';
-// import 'solidity-coverage';
 
 dotenv.config();
 
