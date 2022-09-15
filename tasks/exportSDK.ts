d9bec65b237068a998eb78ce9ca06742858edccd
0xRektora <13756173+0xRektora@users.noreply.github.com>
2022-09-15 11:54:33 -0400

    chore: Refactored `exportSDK` task

--- tasks/exportSDK.ts
+++ tasks/exportSDK.ts
@@ -1,58 +1,48 @@
-import fs from 'fs';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
-import { glob, runTypeChain } from 'typechain';
-import writeJsonFile from 'write-json-file';
-import { getDeployments } from '../scripts/getDeployment-script';
+import { API } from 'tapioca-sdk';
+import { TContract, TProjectDeployment } from 'tapioca-sdk/dist/api/exportSDK';
+
+export const getDeployments = async (_hre: HardhatRuntimeEnvironment) => {
+    const { deployments } = _hre;
+    const all = await deployments.all();
+    return Promise.all(
+        Object.keys(all).map(async (e) => ({
+            name: e,
+            address: all[e].address,
+        })),
+    );
+};
+
 /**
  * Script used to generate typings for the tapioca-sdk
  * https://github.com/Tapioca-DAO/tapioca-sdk
  */
 export const exportSDK__task = async (
-    taskArgs: { mainnet?: boolean },
+    taskArgs: any,
     hre: HardhatRuntimeEnvironment,
 ) => {
-    const cwd = process.cwd();
-    const { mainnet } = taskArgs;
-
-    const __deployments = { prev: {} };
-    try {
-        __deployments.prev = JSON.parse(
-            fs.readFileSync('tapioca-sdk/src/addresses.json', 'utf-8'),
-        );
-    } catch (e) {}
-
-    if (mainnet) {
-        const deployments = {
-            ...__deployments.prev,
-            [await hre.getChainId()]: await getDeployments(hre),
-        };
-        await writeJsonFile('tapioca-sdk/src/addresses.json', deployments);
-    }
-    // We are looking at
-    // BeachBar, Mixologist, MixologistHelper;
-    const allFiles = glob(cwd, [
-        `${hre.config.paths.artifacts}/**/!(*.dbg).json`,
-    ]).filter((e) =>
-        [
+    const _deployments: TProjectDeployment = {
+        [(await hre.getChainId()) as keyof TProjectDeployment]: (
+            await getDeployments(hre)
+        ).map(
+            (e): TContract => ({
+                address: e.address,
+                meta: {},
+                name: e.name,
+            }),
+        ),
+    };
+    await API.exportSDK.run({
+        projectCaller: 'TapiocaZ',
+        contractNames: [
             'YieldBox',
             'BeachBar',
             'Mixologist',
             'MixologistHelper',
             'ERC20',
             'ERC20Mock',
-        ].some((v) => e.split('/').slice(-1)[0] === v.concat('.json')),
-    );
-
-    await runTypeChain({
-        cwd,
-        filesToProcess: allFiles,
-        allFiles,
-        outDir: 'tapioca-sdk/src/typechain/Tapioca-Bar',
-        target: 'ethers-v5',
-        flags: {
-            alwaysGenerateOverloads: true,
-            environment: 'hardhat',
-            discriminateTypes: false,
-        },
+        ],
+        artifactPath: hre.config.paths.artifacts,
+        _deployments,
     });
 };
--- tasks/getBeachBarMarkets.ts
+++ tasks/getBeachBarMarkets.ts
@@ -1,5 +1,24 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
-import { getBeachBarMarkets } from '../scripts/scriptUtils';
+
+export const getBeachBarMarkets = async (hre: HardhatRuntimeEnvironment) => {
+    const beachBarAddr = (await hre.deployments.get('BeachBar')).address;
+    const beachBar = await hre.ethers.getContractAt('BeachBar', beachBarAddr);
+
+    const addresses = await beachBar.tapiocaMarkets();
+    const markets = [];
+
+    for (const address of addresses) {
+        const mixologist = await hre.ethers.getContractAt(
+            'Mixologist',
+            address,
+        );
+        markets.push({
+            [await mixologist.name()]: address,
+        });
+    }
+
+    return markets;
+};
 
 export const getBeachBarMarkets__task = async (
     args: any,
