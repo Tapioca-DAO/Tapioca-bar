8053b4373b70f1d1a5df2db3f811841f459492ef
cryptotechmaker <grigore.cosmin@gmail.com>
2022-12-05 09:45:36 +0200

    removed strategies from this repo

--- contracts/strategies/balancer/IComposableStablePool.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import '@boringcrypto/boring-solidity/contracts/interfaces/IERC20.sol';
-import '@boringcrypto/boring-solidity/contracts/libraries/BoringERC20.sol';
-
-interface IComposableStablePool is IERC20 {
-    function getTokenRate(address token) external view returns (uint256);
-}
--- contracts/strategies/curve/ICurveMinter.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-interface ICurveMinter {
-    // solhint-disable-next-line var-name-mixedcase
-    function mint(address _gauge_addr) external;
-}
