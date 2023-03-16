f80bfe44f2a494cdd4db44bcbfb4aca476d3d47a
0xRektora <13756173+0xRektora@users.noreply.github.com>
2023-03-16 17:10:26 -0400

    fix: Tasks

--- hardhat.tasks.ts
+++ hardhat.tasks.ts
@@ -1,248 +1,229 @@
-// import '@nomiclabs/hardhat-ethers';
-// import { task } from 'hardhat/config';
-// import { deployMarket__task } from './tasks/deployMarket';
-// import { deployBigBang__task } from './tasks/deploy/deployBigBang';
-// import { exportSDK__task } from './tasks/exportSDK';
-// import { getSingularityMarkets__task } from './tasks/getSingularityMarkets';
-// import { getBigBangMarkets__task } from './tasks/views/getBigBangMarkets';
-// import {
-//     getLocalDeployments__task,
-//     getSDKDeployments__task,
-// } from './tasks/getDeployments';
-// import { setBorrowCap__task } from './tasks/execs/setBorrowCap';
-// import { registerYieldBoxAsset__task } from './tasks/registerYbAssets';
-// import { setLiquidationQueueBidSwapper__task } from './tasks/setLiquidationQueueBidSwapper';
-// import { setLiquidationQueueExecutionSwapper__task } from './tasks/setLiquidationQueueExecutionSwapper';
-// import { setLiquidationQueue__task } from './tasks/setLiquidationQueue';
+import '@nomiclabs/hardhat-ethers';
+import { task } from 'hardhat/config';
+import { deployBigBang__task } from './tasks/deploy/deployBigBang';
 
-// import { getParticipantSingularityInfo__task } from './tasks/getParticipantSingularityInfo';
-// import { getParticipantBigBangInfo__task } from './tasks/getParticipantBigBangInfo';
+import { setBorrowCap__task } from './tasks/execs/setBorrowCap';
 
-// import { getSingularityTotals__task } from './tasks/getSingularityTotals';
-// import { getBigBangTotals__task } from './tasks/getBigBangTotals';
+import { airdropGas__task } from './tasks/airdropGas';
+import { deployYbStrats__task } from './tasks/deploy/01-deployEmptyYieldBoxStrategy';
+import { deployOracleMock__task } from './tasks/deploy/04-deployOracleMock';
+import { setLiquidationQueue__task } from './tasks/execs/setLiquidationQueue';
+import { setLiquidationQueueBidSwapper__task } from './tasks/execs/setLiquidationQueueBidSwapper';
+import { setLiquidationQueueExecutionSwapper__task } from './tasks/execs/setLiquidationQueueExecutionSwapper';
+import { setProxyAdapterParams__task } from './tasks/execs/setProxyAdapterParams';
+import { setProxyTrustedRemote__task } from './tasks/execs/setProxyTrustedRemote';
+import { registerYbAssets__task } from './tasks/registerYbAssets';
+import { hasStoredPayload__task } from './tasks/test-hasStoredPayload';
+import { otherChainBorrow__task } from './tasks/test-otherChainBorrow';
+import { retryPayload__task } from './tasks/test-retryPayload';
+import { sameChainBorrow__task } from './tasks/test-sameChainBorrow';
+import { getBigBangMarkets__task } from './tasks/views/getBigBangMarkets';
+import { getBigBangTotals__task } from './tasks/views/getBigBangTotals';
+import { getParticipantBigBangInfo__task } from './tasks/views/getParticipantBigBangInfo';
+import { getParticipantSingularityInfo__task } from './tasks/views/getParticipantSingularityInfo';
+import { getSingularityMarkets__task } from './tasks/views/getSingularityMarkets';
+import { getSingularityTotals__task } from './tasks/views/getSingularityTotals';
+import { whitelistSingularity__task } from './tasks/whitelistSingularity';
+import { deployMarket__task } from './tasks/deploy/___deployMarket';
+import {
+    getLocalDeployments__task,
+    getSDKDeployments__task,
+} from './tasks/views/getDeployments';
 
-// import { deployOracleMock__task } from './tasks/deployOracleMock';
-// import { deployYbStrategy__task } from './tasks/deployEmptyYieldBoxStrategy';
-// import { setProxyTrustedRemote__task } from './tasks/setProxyTrustedRemote';
-// import { setProxyAdapterParams__task } from './tasks/setProxyAdapterParams';
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
+    const accounts = await hre.ethers.getSigners();
 
-// import { sameChainBorrow__task } from './tasks/test-sameChainBorrow';
-// import { otherChainBorrow__task } from './tasks/test-otherChainBorrow';
-// import { hasStoredPayload__task } from './tasks/test-hasStoredPayload';
-// import { retryPayload__task } from './tasks/test-retryPayload';
-// import { configurePacketTypes__task } from './tasks/configurePacketTypes';
-// import { whitelistSingularity__task } from './tasks/whitelistSingularity';
-// import { airdropGas__task } from './tasks/airdropGas';
+    for (const account of accounts) {
+        console.log(account.address);
+    }
+});
 
-// task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
-//     const accounts = await hre.ethers.getSigners();
+task(
+    'singularityMarkets',
+    'Display the list of deployed Singularity markets for the current chain ID.',
+    getSingularityMarkets__task,
+);
+task(
+    'bigBangMarkets',
+    'Display the list of deployed BigBang markets for the current chain ID.',
+    getBigBangMarkets__task,
+);
 
-//     for (const account of accounts) {
-//         console.log(account.address);
-//     }
-// });
+task(
+    'deployMarket',
+    'Deploy a Singularity market, a Liquidation Queue and initialize it.',
+    deployMarket__task,
+)
+    .addParam('name', 'Market name')
+    .addParam('exchangeRatePrecision', 'Collateral decimals');
 
-// task(
-//     'exportSDK',
-//     'Generate and export the typings and/or addresses for the SDK. May deploy contracts.',
-//     exportSDK__task,
-// ).addOptionalParam('tag', 'Tag to export');
+task('deployBigBang', 'Deploy a BigBang market', deployBigBang__task)
+    .addParam('name', 'Market name')
+    .addParam('exchangeRatePrecision', 'Collateral decimals');
 
-// task(
-//     'singularityMarkets',
-//     'Display the list of deployed Singularity markets for the current chain ID.',
-//     getSingularityMarkets__task,
-// );
-// task(
-//     'bigBangMarkets',
-//     'Display the list of deployed BigBang markets for the current chain ID.',
-//     getBigBangMarkets__task,
-// );
+task('deployOracleMock', 'Deploy Oracle mock', deployOracleMock__task).addParam(
+    'name',
+    'Market name',
+);
 
-// task(
-//     'deployMarket',
-//     'Deploy a Singularity market, a Liquidation Queue and initialize it.',
-//     deployMarket__task,
-// )
-//     .addParam('name', 'Market name')
-//     .addParam('exchangeRatePrecision', 'Collateral decimals');
+task(
+    'deployYbStrats',
+    'Deploy a bunch of ERC20WithoutStrategy for YieldBox',
+    deployYbStrats__task,
+)
+    .addParam('yieldbox', 'YieldBox address')
+    .addParam('token', 'ERC20 token address');
 
-// task('deployBigBang', 'Deploy a BigBang market', deployBigBang__task)
-//     .addParam('name', 'Market name')
-//     .addParam('exchangeRatePrecision', 'Collateral decimals');
+task(
+    'getLocalDeployments',
+    'Print a list of locally deployed contracts.',
+    getLocalDeployments__task,
+);
 
-// task('deployOracleMock', 'Deploy Oracle mock', deployOracleMock__task).addParam(
-//     'name',
-//     'Market name',
-// );
+task(
+    'getSDKDeployments',
+    'Print a list of SDK deployed contract.',
+    getSDKDeployments__task,
+);
 
-// task(
-//     'deployYbStrategy',
-//     'Deploy ERC20WithoutStrategy for YieldBox',
-//     deployYbStrategy__task,
-// )
-//     .addParam('yieldbox', 'YieldBox address')
-//     .addParam('token', 'ERC20 token address');
+task(
+    'getParticipantSingularityInfo',
+    'Returns lend & borrow details for a specific address',
+    getParticipantSingularityInfo__task,
+)
+    .addParam('singularity', 'Singularity address')
+    .addParam('participant', 'User address');
 
-// task(
-//     'getLocalDeployments',
-//     'Print a list of locally deployed contracts.',
-//     getLocalDeployments__task,
-// );
+task(
+    'getParticipantBigBangInfo',
+    'Returns lend & borrow details for a specific address',
+    getParticipantBigBangInfo__task,
+)
+    .addParam('singularity', 'Singularity address')
+    .addParam('participant', 'User address');
 
-// task(
-//     'getSDKDeployments',
-//     'Print a list of SDK deployed contract.',
-//     getSDKDeployments__task,
-// );
+task(
+    'getSingularityTotals',
+    'Returns Singularity totals info',
+    getSingularityTotals__task,
+).addParam('singularity', 'Singularity address');
+task(
+    'getBigBangTotals',
+    'Returns BigBang totals info',
+    getBigBangTotals__task,
+).addParam('bigBang', 'BigBang address');
 
-// task(
-//     'getParticipantSingularityInfo',
-//     'Returns lend & borrow details for a specific address',
-//     getParticipantSingularityInfo__task,
-// )
-//     .addParam('singularity', 'Singularity address')
-//     .addParam('participant', 'User address');
+task('setBorrowCap', 'Set borrow cap for Singularity', setBorrowCap__task)
+    .addParam('singularity', 'Singularity address', ' ')
+    .addParam('bigBang', 'BigBang address', ' ')
+    .addParam('cap', 'Borrow cap value');
 
-// task(
-//     'getParticipantBigBangInfo',
-//     'Returns lend & borrow details for a specific address',
-//     getParticipantBigBangInfo__task,
-// )
-//     .addParam('singularity', 'Singularity address')
-//     .addParam('participant', 'User address');
+task(
+    'setLiquidationQueueBidSwapper',
+    'Updates LiquidationQueue bid swapper',
+    setLiquidationQueueBidSwapper__task,
+)
+    .addParam('singularity', 'Singularity address')
+    .addParam('swapper', 'Swapper address');
 
-// task(
-//     'getSingularityTotals',
-//     'Returns Singularity totals info',
-//     getSingularityTotals__task,
-// ).addParam('singularity', 'Singularity address');
-// task(
-//     'getBigBangTotals',
-//     'Returns BigBang totals info',
-//     getBigBangTotals__task,
-// ).addParam('bigBang', 'BigBang address');
+task(
+    'setLiquidationQueueExecutionSwapper',
+    'Updates LiquidationQueue execution swapper',
+    setLiquidationQueueExecutionSwapper__task,
+)
+    .addParam('singularity', 'Singularity address')
+    .addParam('swapper', 'Swapper address');
 
-// task('setBorrowCap', 'Set borrow cap for Singularity', setBorrowCap__task)
-//     .addParam('singularity', 'Singularity address', ' ')
-//     .addParam('bigBang', 'BigBang address', ' ')
-//     .addParam('cap', 'Borrow cap value');
+task(
+    'setLiquidationQueue',
+    'Updates LiquidationQueue',
+    setLiquidationQueue__task,
+)
+    .addParam('singularity', 'Singularity address')
+    .addParam('liquidationQueue', 'LiquidationQueue address')
+    .addParam('meta', 'LiquidationQueue meta object');
 
-// task(
-//     'setLiquidationQueueBidSwapper',
-//     'Updates LiquidationQueue bid swapper',
-//     setLiquidationQueueBidSwapper__task,
-// )
-//     .addParam('singularity', 'Singularity address')
-//     .addParam('swapper', 'Swapper address');
+task(
+    'registerYbAssets',
+    'Register a bunch of YieldBox assets',
+    registerYbAssets__task,
+);
 
-// task(
-//     'setLiquidationQueueExecutionSwapper',
-//     'Updates LiquidationQueue execution swapper',
-//     setLiquidationQueueExecutionSwapper__task,
-// )
-//     .addParam('singularity', 'Singularity address')
-//     .addParam('swapper', 'Swapper address');
+task(
+    'setProxyTrustedRemote',
+    'Calls setProxyTrustedRemote on MarketsProxy contract',
+    setProxyTrustedRemote__task,
+)
+    .addParam('chain', 'LZ destination chain id for trusted remotes')
+    .addParam('dst', 'MarketsProxy destination address')
+    .addParam('src', 'MarketsProxy source address');
 
-// task(
-//     'setLiquidationQueue',
-//     'Updates LiquidationQueue',
-//     setLiquidationQueue__task,
-// )
-//     .addParam('singularity', 'Singularity address')
-//     .addParam('liquidationQueue', 'LiquidationQueue address')
-//     .addParam('meta', 'LiquidationQueue meta object');
+task(
+    'setProxyAdapterParams',
+    'Sets setUseCustomAdapterParams on MarketsProxy contract',
+    setProxyAdapterParams__task,
+).addOptionalParam(
+    'lzDestinationId',
+    'Destination LZ chain id to set min gas for',
+);
 
-// task(
-//     'registerYieldBoxAsset',
-//     'Register YieldBox asset',
-//     registerYieldBoxAsset__task,
-// ).addParam('address', 'Asset address');
+task(
+    'sameChainBorrow',
+    'Deposits, adds collateral and borrows from the same chain',
+    sameChainBorrow__task,
+)
+    .addParam('market', 'Singularity address')
+    .addParam('marketHelper', 'MarketsHelper address')
+    .addParam('collateralAmount', 'Collateral amount to add')
+    .addParam('borrowAmount', 'Borrow amount to get');
 
-// task(
-//     'setProxyTrustedRemote',
-//     'Calls setProxyTrustedRemote on MarketsProxy contract',
-//     setProxyTrustedRemote__task,
-// )
-//     .addParam('chain', 'LZ destination chain id for trusted remotes')
-//     .addParam('dst', 'MarketsProxy destination address')
-//     .addParam('src', 'MarketsProxy source address');
+task(
+    'otherChainBorrow',
+    'Deposits, adds collateral and borrows from the another chain',
+    otherChainBorrow__task,
+)
+    .addParam('srcLzChainId', 'LZ source chain id')
+    .addParam('destLzChainId', 'LZ destination chain id')
+    .addParam('extraGas', 'Extra gas for LZ send tx')
+    .addParam('assetId', 'Destination YieldBox asset id')
+    .addParam('zroAddress', 'ZRO address')
+    .addParam('depositGas', 'ETH sent for tx for YB deposit')
+    .addParam('singularityGas', 'ETH sent for tx for Singularity operations')
+    .addParam('airdropGas', 'ETH airdropped for withdraw operation')
+    .addParam('depositAmount', 'ETH sent for tx')
+    .addParam('oft', 'Tapioca OFT address')
+    .addParam('proxy', 'Proxy contract address')
+    .addParam('collateralShare', 'Collateral shares to add')
+    .addParam('borrowAmount', 'Asset amount to borrow')
+    .addParam('withdraw', 'True/false')
+    .addParam('singularityDestination', 'Singularity destination');
 
-// task(
-//     'setProxyAdapterParams',
-//     'Sets setUseCustomAdapterParams on MarketsProxy contract',
-//     setProxyAdapterParams__task,
-// ).addOptionalParam(
-//     'lzDestinationId',
-//     'Destination LZ chain id to set min gas for',
-// );
+task(
+    'hasStoredPayload',
+    'Check if endpoint has a stored payload',
+    hasStoredPayload__task,
+)
+    .addParam('srcChainId', 'LZ source chain id')
+    .addParam('srcAddress', 'Source address')
+    .addParam('dstAddress', 'Destination address')
+    .addParam('lzEndpoint', 'LZ endpoint address');
 
-// task(
-//     'sameChainBorrow',
-//     'Deposits, adds collateral and borrows from the same chain',
-//     sameChainBorrow__task,
-// )
-//     .addParam('market', 'Singularity address')
-//     .addParam('marketHelper', 'MarketsHelper address')
-//     .addParam('collateralAmount', 'Collateral amount to add')
-//     .addParam('borrowAmount', 'Borrow amount to get');
+task('retryPayload', 'Retry a failed payload', retryPayload__task)
+    .addParam('srcChainId', 'LZ source chain id')
+    .addParam('srcAddress', 'Source address represented as bytes')
+    .addParam('dstAddress', 'Destination address')
+    .addParam('blockHash', 'Blockhash where to take the event from bytes')
+    .addParam('lzEndpoint', 'LZ endpoint address');
 
-// task(
-//     'otherChainBorrow',
-//     'Deposits, adds collateral and borrows from the another chain',
-//     otherChainBorrow__task,
-// )
-//     .addParam('srcLzChainId', 'LZ source chain id')
-//     .addParam('destLzChainId', 'LZ destination chain id')
-//     .addParam('extraGas', 'Extra gas for LZ send tx')
-//     .addParam('assetId', 'Destination YieldBox asset id')
-//     .addParam('zroAddress', 'ZRO address')
-//     .addParam('depositGas', 'ETH sent for tx for YB deposit')
-//     .addParam('singularityGas', 'ETH sent for tx for Singularity operations')
-//     .addParam('airdropGas', 'ETH airdropped for withdraw operation')
-//     .addParam('depositAmount', 'ETH sent for tx')
-//     .addParam('oft', 'Tapioca OFT address')
-//     .addParam('proxy', 'Proxy contract address')
-//     .addParam('collateralShare', 'Collateral shares to add')
-//     .addParam('borrowAmount', 'Asset amount to borrow')
-//     .addParam('withdraw', 'True/false')
-//     .addParam('singularityDestination', 'Singularity destination');
+task(
+    'whitelistSingularity',
+    'Whitelist singularity status on SGLProxy',
+    whitelistSingularity__task,
+)
+    .addParam('singularity', 'SGL address')
+    .addParam('sglProxy', 'SGLProxy addressF');
 
-// task(
-//     'hasStoredPayload',
-//     'Check if endpoint has a stored payload',
-//     hasStoredPayload__task,
-// )
-//     .addParam('srcChainId', 'LZ source chain id')
-//     .addParam('srcAddress', 'Source address')
-//     .addParam('dstAddress', 'Destination address')
-//     .addParam('lzEndpoint', 'LZ endpoint address');
-
-// task('retryPayload', 'Retry a failed payload', retryPayload__task)
-//     .addParam('srcChainId', 'LZ source chain id')
-//     .addParam('srcAddress', 'Source address represented as bytes')
-//     .addParam('dstAddress', 'Destination address')
-//     .addParam('blockHash', 'Blockhash where to take the event from bytes')
-//     .addParam('lzEndpoint', 'LZ endpoint address');
-
-// task(
-//     'configurePacketTypes',
-//     'Cofigures min destination gas and the usage of custom adapters',
-//     configurePacketTypes__task,
-// )
-//     .addParam('dstLzChainId', 'LZ destination chain id for trusted remotes')
-//     .addParam('contract', 'Contract name: USD0, MarketsProxy')
-//     .addParam('src', 'tOFT address');
-
-// task(
-//     'whitelistSingularity',
-//     'Whitelist singularity status on SGLProxy',
-//     whitelistSingularity__task,
-// )
-//     .addParam('singularity', 'SGL address')
-//     .addParam('sglProxy', 'SGLProxy addressF');
-
-// task('airdropGas', 'Airdrop gas to msg.sender', airdropGas__task)
-//     .addParam('amount', 'Amount of gas to airdrop')
-//     .addParam('dstChain', 'Destination chain id')
-//     .addParam('dstAddress', 'Destination address');
+task('airdropGas', 'Airdrop gas to msg.sender', airdropGas__task)
+    .addParam('amount', 'Amount of gas to airdrop')
+    .addParam('dstChain', 'Destination chain id')
+    .addParam('dstAddress', 'Destination address');
