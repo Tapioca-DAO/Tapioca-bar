f75b7e8e474ac3a68c01ff4bc6a43ef74bb63ad1
0xRektora <13756173+0xRektora@users.noreply.github.com>
2023-04-24 10:47:56 -0400

    chore: `.solcover`

--- .solcover.js
+++ .solcover.js
@@ -1,20 +1,8 @@
 module.exports = {
     skipFiles: [
         'bar/enums',
-        'bar/AssetRegister.sol',
-        'bar/ERC1155.sol',
-        'bar/ERC1155TokenReceiver.sol',
-        'bar/NativeTokenFactory.sol',
-        'bar/YieldBox.sol',
-        'bar/YieldBoxRebase.sol',
-        'bar/YieldBoxURIBuilder.sol',
-        'mocks',
-        'liquidationQueue/mocks',
-        'mixologist/legacy',
-        'libraries',
-        'swappers/MultiSwapper.sol',
-        'usd0/mocks/LZEndpointMock.sol',
+        'TickMath',
+        'swappers/libraries/TickMath'
     ],
-    norpc: true,
-    isTruffle: false,
+    configureYulOptimizer: true,
 };
