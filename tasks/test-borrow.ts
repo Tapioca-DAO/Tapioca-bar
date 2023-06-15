40e15f01da7090b19843fe9b5bb9c529f8d60565
0xRektora <13756173+0xRektora@users.noreply.github.com>
2023-06-15 19:53:57 -0400

    patch: Adapted task to  newest changes

--- tasks/test-borrow.ts
+++ tasks/test-borrow.ts
@@ -4,12 +4,17 @@ import { getSingularityContract } from './utils';
 import inquirer from 'inquirer';
 
 import TapiocaOFTArtifact from '../gitsub_tapioca-sdk/src/artifacts/tapiocaz/TapiocaOFT.json';
-import { TapiocaOFT__factory } from '../gitsub_tapioca-sdk/src/typechain/TapiocaZ/factories/TapiocaOFT__factory';
+import { TapiocaOFT__factory } from '../gitsub_tapioca-sdk/src/typechain/TapiocaZ/factories/tOFT/TapiocaOFT__factory';
 import { TContract } from 'tapioca-sdk/dist/shared';
 import { Singularity, USDO__factory } from '../typechain';
 import { BaseTOFT } from '../gitsub_tapioca-sdk/src/typechain/TapiocaZ/TapiocaOFT';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { splitSignature } from 'ethers/lib/utils';
+import MagnetarV2Artifacts from '../gitsub_tapioca-sdk/src/artifacts/tapioca-periphery/MagnetarV2.json';
+import ERC20MockArtifacts from '../gitsub_tapioca-sdk/src/artifacts/tapioca-mocks/ERC20Mock.json';
+import { ERC20Mock } from '../gitsub_tapioca-sdk/src/typechain/tapioca-mocks/ERC20Mock';
+import { MagnetarV2 } from '../gitsub_tapioca-sdk/src/typechain/tapioca-periphery/Magnetar/MagnetarV2';
+import { TAPIOCA_PROJECTS_NAME } from '../gitsub_tapioca-sdk/src/api/config';
 
 export const testCrossChainBorrow__task = async (
     {},
@@ -37,11 +42,15 @@ export const testCrossChainBorrow__task = async (
         (await hre.ethers.getContractFactoryFromArtifact(
             TapiocaOFTArtifact,
         )) as TapiocaOFT__factory
-    ).connect(deployer);
+    ).connect(deployer) as TapiocaOFT__factory;
 
     // Load TOFT on fromChain
     const toft__from__dep = hre.SDK.db
-        .loadGlobalDeployment(tag, 'tapiocaz', fromChain.chainId)
+        .loadGlobalDeployment(
+            tag,
+            TAPIOCA_PROJECTS_NAME.TapiocaZ,
+            fromChain.chainId,
+        )
         .find((e) => e.meta.isToftHost === true);
     if (!toft__from__dep)
         throw new Error(`[-] No TOFT found for chain ${fromChain.name}`);
@@ -50,7 +59,11 @@ export const testCrossChainBorrow__task = async (
 
     // Load USDO on fromChain
     const usdo__from__dep = hre.SDK.db
-        .loadGlobalDeployment(tag, 'tapioca-bar', fromChain.chainId)
+        .loadGlobalDeployment(
+            tag,
+            TAPIOCA_PROJECTS_NAME.TapiocaBar,
+            fromChain.chainId,
+        )
         .find((e) => e.name.toLowerCase() === 'usdo');
 
     if (!usdo__from__dep)
@@ -63,7 +76,11 @@ export const testCrossChainBorrow__task = async (
 
     // Load USDO on toChain
     const usdo__to__dep = hre.SDK.db
-        .loadGlobalDeployment(tag, 'tapioca-bar', toChain.chainId)
+        .loadGlobalDeployment(
+            tag,
+            TAPIOCA_PROJECTS_NAME.TapiocaBar,
+            toChain.chainId,
+        )
         .find((e) => e.name.toLowerCase() === 'usdo');
 
     if (!usdo__to__dep)
@@ -104,15 +121,17 @@ export const testCrossChainBorrow__task = async (
         await hre.ethers.getContractAt('Singularity', sgl__dep.address)
     ).connect(toNetwork);
 
-    // Load market helper
-    const marketsHelper__dep = localToDeployments.find(
-        (e) => e.name.toLowerCase() === 'marketshelper',
+    // Load Magnetar
+    const magnetar__dep = hre.SDK.db.getGlobalDeployment(
+        TAPIOCA_PROJECTS_NAME.Generic,
+        fromChain.chainId,
+        'Magnetar',
     );
-    if (!marketsHelper__dep) throw new Error('[-] No MarketsHelper found');
-    const marketsHelper = await hre.ethers.getContractAt(
-        'MarketsHelper',
-        marketsHelper__dep.address,
-    );
+    if (!magnetar__dep) throw new Error('[-] No Magnetar found');
+    const magnetar = (await hre.ethers.getContractAtFromArtifact(
+        MagnetarV2Artifacts,
+        magnetar__dep.address,
+    )) as MagnetarV2;
 
     // ---------------------------------- Ask for values ----------------------------------
     const collateralToDeposit = hre.ethers.utils.parseEther(
@@ -145,7 +164,7 @@ export const testCrossChainBorrow__task = async (
         'PermitBorrow',
         toNetwork,
         singularity,
-        marketsHelper.address,
+        magnetar.address,
         permitBorrowAmount,
         deadline,
     );
@@ -154,7 +173,7 @@ export const testCrossChainBorrow__task = async (
         deadline,
         permitBorrow: true,
         owner: deployer.address,
-        spender: marketsHelper.address,
+        spender: magnetar.address,
         value: permitBorrowAmount,
         r: permitBorrow.r,
         s: permitBorrow.s,
@@ -168,7 +187,7 @@ export const testCrossChainBorrow__task = async (
         'Permit',
         toNetwork,
         singularity,
-        marketsHelper.address,
+        magnetar.address,
         permitLendAmount,
         deadline,
         {
@@ -179,7 +198,7 @@ export const testCrossChainBorrow__task = async (
         deadline,
         permitBorrow: false,
         owner: deployer.address,
-        spender: marketsHelper.address,
+        spender: magnetar.address,
         value: permitLendAmount,
         r: permitLend.r,
         s: permitLend.s,
@@ -209,14 +228,14 @@ export const testCrossChainBorrow__task = async (
             2, //it needs to be 2
             1_000_000, //extra gas limit; min 200k
             withdrawFees, //amount of eth to airdrop
-            marketsHelper.address,
+            magnetar.address,
         ],
     );
 
-    const erc20 = await hre.ethers.getContractAt(
-        'ERC20Mock',
+    const erc20 = (await hre.ethers.getContractAtFromArtifact(
+        ERC20MockArtifacts,
         await toftFrom.erc20(),
-    );
+    )) as ERC20Mock;
 
     // check allowance
     if (
@@ -239,6 +258,16 @@ export const testCrossChainBorrow__task = async (
         ).wait(3);
     }
 
+    // Wrap
+    console.log('[+] Wrapping');
+    await (
+        await toftFrom.wrap(
+            deployer.address,
+            deployer.address,
+            collateralToDeposit,
+        )
+    ).wait(3);
+
     // ---------------------------------- Execute borrow OP ----------------------------------
 
     const call = toftFrom.interface.encodeFunctionData('sendToYBAndBorrow', [
@@ -249,10 +278,11 @@ export const testCrossChainBorrow__task = async (
         {
             amount: collateralToDeposit,
             borrowAmount,
+            marketHelper: magnetar.address,
             market: singularity.address,
-            marketHelper: marketsHelper.address,
         },
         {
+            withdraw: true,
             withdrawAdapterParams: hre.ethers.utils.solidityPack(
                 ['uint16', 'uint256'],
                 [1, 200000],
@@ -263,8 +293,6 @@ export const testCrossChainBorrow__task = async (
         },
         {
             extraGasLimit: 1_000_000,
-            strategyDeposit: false,
-            wrap: true,
             zroPaymentAddress: deployer.address,
         },
         [permitBorrowStruct, permitLendStruct],
@@ -295,7 +323,7 @@ export const testCrossChainBorrow__task = async (
             {
                 amount: collateralToDeposit,
                 borrowAmount,
-                marketHelper: marketsHelper.address,
+                marketHelper: magnetar.address,
                 market: singularity.address,
             },
             {
