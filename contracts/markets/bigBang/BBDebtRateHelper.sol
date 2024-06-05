b1c2c99f7795d6527ab94dd98c504ed22e383fc7
cryptotechmaker <grigore.cosmin@gmail.com>
2024-06-05 10:48:19 +0300

    feat(`BB debt rate`): added `BBDebtRateHelper` [`86dtqneet`]

---
+++ contracts/markets/bigBang/BBDebtRateHelper.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.22;
+
+// Tapioca
+import {IBigBangDebtRateHelper} from "tapioca-periph/interfaces/bar/IBigBangDebtRateHelper.sol";
+import {IPenrose} from "tapioca-periph/interfaces/bar/IPenrose.sol";
+import {IBigBang} from "tapioca-periph/interfaces/bar/IBigBang.sol";
+
+
+/*
+
+████████╗ █████╗ ██████╗ ██╗ ██████╗  ██████╗ █████╗ 
+╚══██╔══╝██╔══██╗██╔══██╗██║██╔═══██╗██╔════╝██╔══██╗
+   ██║   ███████║██████╔╝██║██║   ██║██║     ███████║
+   ██║   ██╔══██║██╔═══╝ ██║██║   ██║██║     ██╔══██║
+   ██║   ██║  ██║██║     ██║╚██████╔╝╚██████╗██║  ██║
+   ╚═╝   ╚═╝  ╚═╝╚═╝     ╚═╝ ╚═════╝  ╚═════╝╚═╝  ╚═╝
+   
+*/
+
+
+contract BBDebtRateHelper is IBigBangDebtRateHelper {
+    uint256 internal constant DEBT_PRECISION = 1e18;
+
+    function getDebtRate(DebtRateCall memory data) external override view returns (uint256) {
+        if (data.isMainMarket) return data.penrose.bigBangEthDebtRate(); // default 0.5%
+        if (data.elastic == 0) return data.minDebtRate;
+
+        uint256 _ethMarketTotalDebt = IBigBang(data.penrose.bigBangEthMarket()).getTotalDebt();
+        uint256 _maxDebtPoint = (_ethMarketTotalDebt * data.debtRateAgainstEthMarket) / 1e18;
+
+        if (data.elastic >= _maxDebtPoint) return data.maxDebtRate;
+
+        uint256 debtPercentage = (data.elastic * DEBT_PRECISION) / _maxDebtPoint;
+        uint256 debt = ((data.maxDebtRate - data.minDebtRate) * debtPercentage) / DEBT_PRECISION + data.minDebtRate;
+
+        if (debt > data.maxDebtRate) return data.maxDebtRate;
+
+        return debt;
+    }
+}
