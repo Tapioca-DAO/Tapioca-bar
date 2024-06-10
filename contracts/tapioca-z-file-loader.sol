4d2915a24d5b726ed7a7b358c78b0ae5efbca35e
0xRektora <13756173+0xRektora@users.noreply.github.com>
2024-06-09 23:30:42 -0400

    feat(`foundry`): Loading `tapiocaz` TOFT contract

---
+++ contracts/tapioca-z-file-loader.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.22;
+
+/// THIS FILE IS USED TO LOAD THE TAPIOCA Z CONTRACTS
+/// Comment the imports for faster compilation
+
+import {TOFT} from "tapiocaz/tOFT/TOFT.sol";
