dfb77a22ea0d520018f9aa5989c6c8f9f2d5fd01
0xRektora <13756173+0xRektora@users.noreply.github.com>
2024-06-19 18:42:42 -0400

    feat(`SGL`): Migrated `SGL.init()` code to new `SGLInit` contract `86dtvntc3`

--- contracts/markets/singularity/Singularity.sol
+++ contracts/markets/singularity/Singularity.sol
@@ -9,8 +9,6 @@ import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
 // Tapioca
 import {ILeverageExecutor} from "tapioca-periph/interfaces/bar/ILeverageExecutor.sol";
 import {ITapiocaOracle} from "tapioca-periph/interfaces/periph/ITapiocaOracle.sol";
-import {IYieldBox} from "tapioca-periph/interfaces/yieldbox/IYieldBox.sol";
-import {IPearlmit} from "tapioca-periph/interfaces/periph/IPearlmit.sol";
 import {IPenrose} from "tapioca-periph/interfaces/bar/IPenrose.sol";
 import {Module} from "tapioca-periph/interfaces/bar/IMarket.sol";
 import {MarketStateView} from "../MarketStateView.sol";
@@ -52,15 +50,9 @@ contract Singularity is MarketStateView, SGLCommon {
     /// @notice returns the leverage module
     SGLLeverage public leverageModule;
 
-    // ************** //
-    // *** ERRORS *** //
-    // ************** //
-    error NotValid();
-    error ModuleNotSet();
-    error NotAuthorized();
-    error SameState();
-    error MinLendAmountNotMet();
-
+    /**
+     * Struct are used in SGLInit.sol
+     */
     struct _InitMemoryData {
         IPenrose penrose_;
         ITapiocaOracle _oracle;
@@ -84,125 +76,25 @@ contract Singularity is MarketStateView, SGLCommon {
         uint256 _collateralId;
     }
 
+    // ************** //
+    // *** ERRORS *** //
+    // ************** //
+    error NotValid();
+    error ModuleNotSet();
+    error NotAuthorized();
+    error SameState();
+    error MinLendAmountNotMet();
+
     function totalSupply() public view override(MarketERC20, SGLStorage) returns (uint256) {
         return totalAsset.base;
     }
 
-    /// @notice The init function that acts as a constructor
-    function init(bytes calldata initData) external onlyOnce {
-        (
-            _InitMemoryModulesData memory _initMemoryModulesData,
-            _InitMemoryTokensData memory _initMemoryTokensData,
-            _InitMemoryData memory _initMemoryData
-        ) = abi.decode(initData, (_InitMemoryModulesData, _InitMemoryTokensData, _InitMemoryData));
-
-        penrose = _initMemoryData.penrose_;
-        pearlmit = IPearlmit(_initMemoryData.penrose_.pearlmit());
-        yieldBox = IYieldBox(_initMemoryData.penrose_.yieldBox());
-        _transferOwnership(address(penrose));
-
-        if (address(_initMemoryTokensData._collateral) == address(0)) {
-            revert NotValid();
-        }
-        if (address(_initMemoryTokensData._asset) == address(0)) {
-            revert NotValid();
-        }
-        if (address(_initMemoryData._oracle) == address(0)) revert NotValid();
-
-        _initModules(
-            _initMemoryModulesData._liquidationModule,
-            _initMemoryModulesData._borrowModule,
-            _initMemoryModulesData._collateralModule,
-            _initMemoryModulesData._leverageModule
-        );
-        _initCoreStorage(
-            _initMemoryTokensData._asset,
-            _initMemoryTokensData._assetId,
-            _initMemoryTokensData._collateral,
-            _initMemoryTokensData._collateralId,
-            _initMemoryData._oracle,
-            _initMemoryData._leverageExecutor
-        );
-        _initDefaultValues(
-            _initMemoryData._collateralizationRate,
-            _initMemoryData._liquidationCollateralizationRate,
-            _initMemoryData._exchangeRatePrecision
-        );
-    }
-
-    function _initModules(
-        address _liquidationModule,
-        address _borrowModule,
-        address _collateralModule,
-        address _leverageModule
-    ) private {
-        if (_liquidationModule == address(0)) revert NotValid();
-        if (_collateralModule == address(0)) revert NotValid();
-        if (_borrowModule == address(0)) revert NotValid();
-        if (_leverageModule == address(0)) revert NotValid();
-        assembly {
-            sstore(liquidationModule.slot, _liquidationModule)
-            sstore(collateralModule.slot, _collateralModule)
-            sstore(borrowModule.slot, _borrowModule)
-            sstore(leverageModule.slot, _leverageModule)
-        }
-    }
-
-    function _initCoreStorage(
-        IERC20 _asset,
-        uint256 _assetId,
-        IERC20 _collateral,
-        uint256 _collateralId,
-        ITapiocaOracle _oracle,
-        ILeverageExecutor _leverageExecutor
-    ) private {
-        assembly {
-            // Store the values directly into their respective slots
-            sstore(asset.slot, _asset)
-            sstore(collateral.slot, _collateral)
-            sstore(assetId.slot, _assetId)
-            sstore(collateralId.slot, _collateralId)
-            sstore(oracle.slot, _oracle)
-            sstore(leverageExecutor.slot, _leverageExecutor)
-        }
-    }
-
-    function _initDefaultValues(
-        uint256 _collateralizationRate,
-        uint256 _liquidationCollateralizationRate,
-        uint256 _exchangeRatePrecision
-    ) private {
-        minBorrowAmount = 1e15;
-        minLendAmount = 1e15;
-        collateralizationRate = _collateralizationRate > 0 ? _collateralizationRate : 75000;
-        liquidationCollateralizationRate =
-            _liquidationCollateralizationRate > 0 ? _liquidationCollateralizationRate : 80000;
-        require(
-            liquidationCollateralizationRate > collateralizationRate, "SGL: liquidationCollateralizationRate not valid"
-        );
-       
-        minimumInterestPerSecond = 158548960;
-        maximumInterestPerSecond = 317097920000;
-        interestElasticity = 28800e36; // Half or double in 28800 seconds (8 hours) if linear
-        startingInterestPerSecond = minimumInterestPerSecond;
-        accrueInfo.interestPerSecond = startingInterestPerSecond; // 1% APR, with 1e18 being 100%
-        updateExchangeRate();
-        assembly {
-            //default fees
-            sstore(protocolFee.slot, 10000) // 10%; used for accrual
-            sstore(borrowOpeningFee.slot, 50) // 0.05%
-
-            //liquidation
-            sstore(liquidationMultiplier.slot, 12000) //12%
-            sstore(minLiquidatorReward.slot, 80000) //8e4
-            sstore(maxLiquidatorReward.slot, 90000) //9e4
-            sstore(liquidationBonusAmount.slot, 10000) //1e4
-            sstore(minimumTargetUtilization.slot, 300000000000000000) //3e17
-            sstore(maximumTargetUtilization.slot, 500000000000000000) //5e17
-            sstore(rateValidDuration.slot, 86400) //24 hours
-        }
-
-        EXCHANGE_RATE_PRECISION = _exchangeRatePrecision > 0 ? _exchangeRatePrecision : 1e18;
+    /**
+     * @notice Initializes the Singularity contract via the SGLInit contract
+     */
+    function init(address sglInit, bytes calldata initData) external onlyOnce {
+        (bool success,) = sglInit.delegatecall(abi.encodeWithSignature("init(bytes)", initData));
+        require(success, "Init failed");
     }
 
     // ************************ //
@@ -245,7 +137,7 @@ contract Singularity is MarketStateView, SGLCommon {
         optionNotPaused(PauseType.AddAsset)
         allowedLend(from, share)
         returns (uint256 fraction)
-    {   
+    {
         uint256 _amount = yieldBox.toAmount(assetId, share, false);
         if (_amount <= minLendAmount) revert MinLendAmountNotMet();
 
@@ -275,7 +167,9 @@ contract Singularity is MarketStateView, SGLCommon {
     /// @dev can only be called by the conservator
     /// @param val the new value
     function updatePause(PauseType _type, bool val, bool resetAccrueTimestmap) external {
-        if (!penrose.cluster().hasRole(msg.sender, keccak256("PAUSABLE")) && msg.sender != owner()) revert NotAuthorized();
+        if (!penrose.cluster().hasRole(msg.sender, keccak256("PAUSABLE")) && msg.sender != owner()) {
+            revert NotAuthorized();
+        }
         if (val == pauseOptions[_type]) revert SameState();
         emit PausedUpdated(_type, pauseOptions[_type], val);
         pauseOptions[_type] = val;
