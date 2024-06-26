c10b00c36f685eb19e74c3707e5512f088289de9
cryptotechmaker <grigore.cosmin@gmail.com>
2024-06-26 09:53:54 +0300

    feat(`sGLpMarketLiquidatorReceiver`): added `sGLpMarketLiquidatorReceiver`  and updated `MarketLiquidatorReceiver` constructor args [`86dtwxggf`]

--- contracts/liquidators/sGlpMarketLiquidatorReceiver.sol
+++ contracts/liquidators/sGlpMarketLiquidatorReceiver.sol
@@ -8,6 +8,8 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 // Tapioca
 import {IMarketLiquidatorReceiver} from "tapioca-periph/interfaces/bar/IMarketLiquidatorReceiver.sol";
+import {IGmxRewardRouterV2} from "tapioca-periph/interfaces/external/gmx/IGmxRewardRouterV2.sol";
+import {IGmxGlpManager} from "tapioca-periph/interfaces/external/gmx/IGmxGlpManager.sol";
 import {IZeroXSwapper} from "tapioca-periph/interfaces/periph/IZeroXSwapper.sol";
 import {IWeth9} from "tapioca-periph/interfaces/external/weth/IWeth9.sol";
 import {ICluster} from "tapioca-periph/interfaces/periph/ICluster.sol";
@@ -24,12 +26,15 @@ import {ITOFT} from "tapioca-periph/interfaces/oft/ITOFT.sol";
    
 */
 
-contract MarketLiquidatorReceiver is IMarketLiquidatorReceiver, Ownable, ReentrancyGuard {
+contract sGlpMarketLiquidatorReceiver is IMarketLiquidatorReceiver, Ownable, ReentrancyGuard {
     using SafeERC20 for IERC20;
 
     address public swapper;
     address public immutable weth;
     ICluster public immutable cluster;
+
+    IGmxRewardRouterV2 private immutable glpRewardRouter;
+    IGmxGlpManager private immutable glpManager;
 
     event SwapperAssigned(address indexed oldSwapper, address indexed swapper);
 
@@ -37,21 +42,34 @@ contract MarketLiquidatorReceiver is IMarketLiquidatorReceiver, Ownable, Reentra
     error WhitelistError();
     error NotEnough();
     error SwapFailed();
+    error SellGlpFailed();
     error NotValid();
 
-    constructor(address _weth, ICluster _cluster, address _swapper) {
+    constructor(address _weth, ICluster _cluster, address _swapper, IGmxRewardRouterV2 _glpRewardRouter, IGmxGlpManager _glpManager) {
         if (_weth == address(0)) revert NotValid();
         if (_swapper == address(0)) revert NotValid();
         if (address(_cluster) == address(0)) revert NotValid();
+        if (address(_glpRewardRouter) == address(0)) revert NotValid();
+        if (address(_glpManager) == address(0)) revert NotValid();
         
         weth = _weth;
         swapper = _swapper;
         cluster = _cluster;
+        glpManager = _glpManager;
+        glpRewardRouter = _glpRewardRouter;
     }
 
     struct SSwapData {
         uint256 minAmountOut;
         IZeroXSwapper.SZeroXSwapData data;
+    }
+
+    struct SGlpSwapData {
+        SSwapData zeroXswapData;
+        // Token to swap tsGlp > token > Usdo.
+        address token;
+        // Min amount of tokens to receive after a sell GLP swap
+        uint256 minAmountOut;
     }
 
     /// @notice returns the swapper sell token
@@ -81,30 +99,25 @@ contract MarketLiquidatorReceiver is IMarketLiquidatorReceiver, Ownable, Reentra
         // check if contract received enough collateral
         uint256 collateralBalance = IERC20(tokenIn).balanceOf(address(this));
         if (collateralBalance < collateralAmount) revert NotEnough();
-        SSwapData memory swapData = abi.decode(data, (SSwapData));
+
+        SGlpSwapData memory swapData = abi.decode(data, (SGlpSwapData));
 
         // unwrap TOFT
         uint256 unwrapped = ITOFT(tokenIn).unwrap(address(this), collateralAmount);
+        address sGLP = ITOFT(tokenIn).erc20();
 
-        // get ERC20
-        address erc20 = ITOFT(tokenIn).erc20();
-
-        // if native, wrap it into WETH
-        if (erc20 == address(0)) {
-            IWeth9(weth).deposit{value: unwrapped}();
-            erc20 = weth;
-        }
+        uint256 tokenAmount = _sellGlp(swapData.token, swapData.minAmountOut, sGLP, unwrapped);
 
         // validate swapData;
         // swapTarget & msg.sender whitelist status is validated inside 0xSwapper
-        if (address(swapData.data.sellToken) != erc20) revert NotAuthorized();
-        if (address(swapData.data.buyToken) != tokenOut) revert NotAuthorized();
+        if (address(swapData.zeroXswapData.data.sellToken) != swapData.token) revert NotAuthorized();
+        if (address(swapData.zeroXswapData.data.buyToken) != tokenOut) revert NotAuthorized();
 
         // swap TOFT.erc20() with `tokenOut`
-        IERC20(erc20).safeApprove(swapper, unwrapped);
-        uint256 amountOut = IZeroXSwapper(swapper).swap(swapData.data, unwrapped, swapData.minAmountOut);
-        IERC20(erc20).safeApprove(swapper, 0);
-        if (amountOut < swapData.minAmountOut) revert SwapFailed();
+        IERC20(swapData.token).safeApprove(swapper, tokenAmount);
+        uint256 amountOut = IZeroXSwapper(swapper).swap(swapData.zeroXswapData.data, tokenAmount, swapData.zeroXswapData.minAmountOut);
+        IERC20(swapData.token).safeApprove(swapper, 0);
+        if (amountOut < swapData.zeroXswapData.minAmountOut) revert SwapFailed();
 
         // tokenOut should be USDO
         IERC20(tokenOut).safeTransfer(msg.sender, amountOut);
@@ -117,6 +130,28 @@ contract MarketLiquidatorReceiver is IMarketLiquidatorReceiver, Ownable, Reentra
     function setSwapper(address _swapper) external onlyOwner {
         emit SwapperAssigned(swapper, _swapper);
         swapper = _swapper;
+    }
+
+
+       /**
+     * @dev Sells GLP for `token`. The `token` is chosen off-chain and is computed to be the best to sell GLP for,
+     * for swapping the `token` to USDO.
+     *
+     * @param token Token to swap for GLP
+     * @param minTokenAmountOut Min amount of `token` to receive
+     * @param sGLP sGLP address
+     * @param glpAmount Amount of GLP to swap for `token`
+     *
+     * @return tokenAmount Amount of `token` received
+     */
+    function _sellGlp(address token, uint256 minTokenAmountOut, address sGLP, uint256 glpAmount)
+        private
+        returns (uint256 tokenAmount)
+    {
+        IERC20(sGLP).safeApprove(address(glpManager), glpAmount);
+        tokenAmount = glpRewardRouter.unstakeAndRedeemGlp(token, glpAmount, minTokenAmountOut, address(this));
+        IERC20(sGLP).safeApprove(address(glpManager), 0);
+        if (tokenAmount < minTokenAmountOut) revert SellGlpFailed();
     }
 
     receive() external payable {}
---
+++ tasks/deployBuilds/buildsGLPMarketLiquidatorReceiver.ts
@@ -0,0 +1,17 @@
+import { IDeployerVMAdd } from '@tapioca-sdk//ethers/hardhat/DeployerVM';
+import { SGlpMarketLiquidatorReceiver__factory } from '@typechain/index';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DEPLOYMENT_NAMES } from '../deploy/DEPLOY_CONFIG';
+
+export const buildsGLPMarketLiquidatorReceiver = async (
+    hre: HardhatRuntimeEnvironment,
+    args: Parameters<SGlpMarketLiquidatorReceiver__factory['deploy']>,
+): Promise<IDeployerVMAdd<SGlpMarketLiquidatorReceiver__factory>> => {
+    return {
+        contract: await hre.ethers.getContractFactory(
+            'SGlpMarketLiquidatorReceiver',
+        ),
+        deploymentName: DEPLOYMENT_NAMES.sGlpMARKET_LIQUIDATOR_RECEIVER,
+        args,
+    };
+};
